4f86d70a2056478f2f60baf2224bf0d27e881df7
Christian Caballero <xtian.cr78@gmail.com>
2024-08-31 08:39:37 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ window.onload = function() {
   // Seleccionamos el formulario y el botón de envío
   const form = document.querySelector(".needs-validation");
   const sendButton = document.querySelector(".btn-primary");
+  const h1 = document.querySelector("h1"); // Seleccionamos el título
 
   // Agregamos un evento de escucha al botón de envío
   sendButton.addEventListener("click", e => {
@@ -27,19 +28,88 @@ window.onload = function() {
   });
 
   // Seleccionamos el campo de la tarjeta de crédito
-  const cardInput = document.querySelector("#validationCustom01");
-
-  // Agregamos un evento de escucha al campo de la tarjeta de crédito
-  cardInput.addEventListener("input", () => {
-    // Verificamos si el campo tiene 16 dígitos
-    if (cardInput.value.length === 16) {
-      // Si el campo tiene 16 dígitos, lo marcamos como válido
-      cardInput.classList.add("is-valid");
+  const tarjetaInput = document.querySelector("#validationCustom01");
+  tarjetaInput.addEventListener("input", () => {
+    if (tarjetaInput.value.length === 16) {
+      tarjetaInput.classList.add("is-valid");
     } else {
-      // Si el campo no tiene 16 dígitos, lo marcamos como inválido
-      cardInput.classList.remove("is-valid");
+      tarjetaInput.classList.remove("is-valid");
     }
   });
+
+  // Seleccionamos el campo del CVC
+  const cvcInput = document.querySelector("#validationCustom02");
+  cvcInput.addEventListener("input", () => {
+    if (cvcInput.value.length === 4) {
+      cvcInput.classList.add("is-valid");
+    } else {
+      cvcInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo de monto
+  const montoInput = document.querySelector("#validationCustom03");
+  montoInput.addEventListener("input", () => {
+    if (montoInput.value.length > 0) {
+      montoInput.classList.add("is-valid");
+    } else {
+      montoInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo del nombre
+  const nombreInput = document.querySelector("#validationCustom04");
+  nombreInput.addEventListener("input", () => {
+    if (nombreInput.value.length > 2) {
+      nombreInput.classList.add("is-valid");
+    } else {
+      nombreInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo del apellido
+  const apellidoInput = document.querySelector("#validationCustom05");
+  apellidoInput.addEventListener("input", () => {
+    if (apellidoInput.value.length > 2) {
+      apellidoInput.classList.add("is-valid");
+    } else {
+      apellidoInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo del ciudad
+  const ciudadInput = document.querySelector("#validationCustom06");
+  ciudadInput.addEventListener("input", () => {
+    if (ciudadInput.value.length >= 2) {
+      ciudadInput.classList.add("is-valid");
+    } else {
+      ciudadInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo del pais
+  const paisInput = document.querySelector("#validationCustom07");
+  paisInput.addEventListener("input", () => {
+    if (paisInput.value.length > 3) {
+      paisInput.classList.add("is-valid");
+    } else {
+      paisInput.classList.remove("is-valid");
+    }
+  });
+
+  // Seleccionamos el campo del codPostal
+  const codPostalInput = document.querySelector("#validationCustom02");
+  codPostalInput.addEventListener("input", () => {
+    if (codPostalInput.value.length === 5) {
+      codPostalInput.classList.add("is-valid");
+    } else {
+      codPostalInput.classList.remove("is-valid");
+    }
+  });
+
+  // Mostramos un mensaje de error debajo del título
+  const errorMessage = document.createElement("p");
+  errorMessage.textContent = "hay campos vacios";
 
   console.log("Hello Rigo from the console!");
 };
